c6c8052f15b3ef204780a062df40e818692822c6
godfb <brandon.godfrey@autodesk.com>
2015-10-29 13:30:39 -0700

    fixed order of operations in one of the equations, improved UI

--- Client/js/theForm.js
+++ Client/js/theForm.js
@@ -1,32 +1,24 @@
 var TheForm = React.createClass({
-  propTypes: {
-    onAcronymsSubmit: React.PropTypes.func.isRequired,
-    handleClick: React.PropTypes.func.isRequired
-  },
   render: function() {
     return (
-      <div className="col s12 m8 l6">
-        <form className="col s6">
-            <input onChange={this.submit} placeholder="  Velocity Initial" type="number" ref="vi" className="row"/>
-            <input onChange={this.submit} placeholder="  Velocity Final" type="number" ref="vf" className="row"/>
-            <input onChange={this.submit} placeholder="  Displacement" type="number" ref="d" className="row"/>
-            <input onChange={this.submit} placeholder="  Time" type="number" ref="t" className="row"/>
-            <input onChange={this.submit} placeholder="  Acceleration" type="number" ref="a" className="row"/>
+      <div className="form col s12 m8 l6">
+        <form className="col offset-m2 offset-l3">
+          <div class="input-field">
+            <label for="vi">Velocity Inital</label>
+            <input onChange={this.submit} type="number" ref="vi" id="vi"/>
+          </div>
+            <label for="vf">Velocity Final</label>
+            <input onChange={this.submit} type="number" ref="vf" id="vf"/>
+            <label for="d">Displacement</label>
+            <input onChange={this.submit} type="number" ref="d" id="d"/>
+            <label for="t">Time</label>
+            <input onChange={this.submit} type="number" ref="t" id="t"/>
+            <label for="a">Acceleration</label>
+            <input onChange={this.submit} type="number" ref="a" id="a"/>
             <textArea className="RESULT" disabled ref="result"/>
         </form>
       </div>
     )
-  },
-    handleChange: function(event) {
-        
-        if(event.target.value === "") {
-            this.props.submitClass = "disabled waves-effect waves-light btn animated fadeOut";
-            this.setState({ shouldHide: true });
-        }
-        else {
-            this.props.submitClass = "waves-effect waves-light btn animated fadeIn";
-            this.setState({ shouldHide: false });
-        }
   },
       submit: function(submission){
         var returnObj = {};
@@ -37,6 +29,9 @@ var TheForm = React.createClass({
         postObject.d = React.findDOMNode(this.refs.d).value;
         postObject.t = React.findDOMNode(this.refs.t).value;
         postObject.a = React.findDOMNode(this.refs.a).value;
+        
+        // if (!!postObject.vi)  
+        
         $.ajax({
             url: "/solve",
             type: 'POST',
@@ -44,11 +39,52 @@ var TheForm = React.createClass({
             contentType: 'application/json',
             success: function (response) {
                 console.log('query successful posted:', response);
-                var resultString = "";
-                for (var key in response){
-                  resultString += key + " = " + response[key] + "\r";
-                }
-                React.findDOMNode(self.refs.result).value = resultString;
+                if(response !== false) {
+                  
+                  if (React.findDOMNode(self.refs.vi).value){
+                    React.findDOMNode(self.refs.vi).disabled = false;
+                  }
+                  else{
+                    React.findDOMNode(self.refs.vi).disabled = true;
+                  }
+                  if (React.findDOMNode(self.refs.vf).value){
+                    React.findDOMNode(self.refs.vf).disabled = false;
+                  }
+                  else{
+                    React.findDOMNode(self.refs.vf).disabled = true;
+                  }
+                  if (React.findDOMNode(self.refs.a).value){
+                    React.findDOMNode(self.refs.a).disabled = false;
+                  }
+                  else{
+                    React.findDOMNode(self.refs.a).disabled = true;
+                  }
+                  if (React.findDOMNode(self.refs.t).value){
+                    React.findDOMNode(self.refs.t).disabled = false;
+                  }
+                  else{
+                    React.findDOMNode(self.refs.t).disabled = true;
+                  }
+                  if (React.findDOMNode(self.refs.d).value){
+                    React.findDOMNode(self.refs.d).disabled = false;
+                  }
+                  else{
+                    React.findDOMNode(self.refs.d).disabled = true;
+                  }
+                  
+                  var resultString = "";
+                  for (var key in response){
+                    resultString += key + " = " + response[key] + "\r";
+                  }
+                  React.findDOMNode(self.refs.result).value = resultString;
+              }
+              else {
+                    React.findDOMNode(self.refs.d).disabled = false;
+                    React.findDOMNode(self.refs.a).disabled = false;
+                    React.findDOMNode(self.refs.t).disabled = false;
+                    React.findDOMNode(self.refs.vf).disabled = false;
+                    React.findDOMNode(self.refs.vi).disabled = false;
+              }
             },
             error: function (data) {
               //console.error('failed to post acronym :(');
@@ -57,5 +93,5 @@ var TheForm = React.createClass({
               //console.log('finished posting');
             }
         });
-    }, 
+    }
 });
--- server.js
+++ server.js
@@ -26,66 +26,105 @@ app.post('/solve', function(req, res) {
         var d = req.body.d;
         var t = req.body.t;
         var a = req.body.a;
+        returnObj.vi = req.body.vi;
+        returnObj.vf = req.body.vf;
+        returnObj.d = req.body.d;
+        returnObj.t = req.body.t;
+        returnObj.a = req.body.a;
         
-        if (vi && a && t)
-            returnObj.vf = parseFloat(vi)+parseFloat((a*t));
-        else if(a && t && vf)
-            returnObj.vi = parseFloat(vf)-parseFloat((a*t));
-        else if(t && vf && vi)
-            if (t !== 0)
-                returnObj.a = parseFloat((vf-vi)/parseFloat(t));
-        else if(vf && vi && a)
-            if (a !== 0)
-                returnObj.t = parseFloat((vf-vi)/parseFloat(a));
+        var somethingNew = true;
         
-        if (vi && t && a){
-            returnObj.d = parseFloat(vi*t) + parseFloat(.5 * a * t * t);
-        }
-        else if (t && a && d){
-            if (t !== 0){
-                returnObj.vi = parseFloat(d/t) - parseFloat(a*t/2);
+        var total = 0;
+        
+        while(somethingNew){
+            
+            if (vi && a && t && !vf){
+                returnObj.vf = parseFloat(vi)+parseFloat((a*t));
             }
-        }
-        else if (a && d && vi){
-            if (a !== 0){
-                returnObj.t = parseFloat(Math.sqrt(parseFloat(2*a*d) - parseFloat(1)) + parseFloat(vi)) / a;
+            else if(a && t && vf && !vi){
+                returnObj.vi = parseFloat(vf)-parseFloat((a*t));
             }
-            else if (a === 0){
-                returnObj.t = Math.abs(parseFloat(vi*d));
+            else if(t && vf && vi && !a){
+                if (t !== 0){
+                    returnObj.a = parseFloat((vf-vi)/parseFloat(t));
+                }
             }
-        }
-        else if (d && t && vi){
-            returnObj.a = parseFloat(2*d) - (parseFloat(2*vi*t)) / parseFloat(t*t);
+            else if(vf && vi && a && !t){
+                if (a !== 0){
+                    returnObj.t = parseFloat((vf-vi)/parseFloat(a));
+                }
+            }
+            
+            if (vi && t && a && !d){
+                returnObj.d = parseFloat(vi*t) + parseFloat(.5 * a * t * t);
+            }
+            else if (t && a && d && !vi){
+                if (t !== 0){
+                    returnObj.vi = parseFloat(d/t) - parseFloat(a*t/2);
+                }
+            }
+            else if (a && d && vi && !t){
+                if (a !== 0){
+                    returnObj.t = parseFloat(Math.sqrt(parseFloat(2*a*d) - parseFloat(1)) + parseFloat(vi)) / a;
+                }
+                else if (a === 0){
+                    returnObj.t = Math.abs(parseFloat(vi*d));
+                }
+            }
+            else if (d && t && vi && !a){
+                returnObj.a = parseFloat(2*d) - (parseFloat(2*vi*t)) / parseFloat(t*t);
+            }
+            
+            if (vi && vf && t && !d){
+                returnObj.d = parseFloat(parseFloat(parseFloat(vi) + parseFloat(vf)) / 2) * parseFloat(t);
+            }
+            else if (vf && t && d && !vi){
+                returnObj.vi = parseFloat(parseFloat(parseFloat(2 * d) / parseFloat(t)) - parseFloat(vf));
+            }
+            else if (t && d && vi && !vf){
+                returnObj.vf = parseFloat(parseFloat(parseFloat(2 * d) / parseFloat(t)) - parseFloat(vi));
+            }
+            else if (d && vi && vf && !t){
+                returnObj.t = parseFloat(parseFloat(2 * d) / parseFloat(parseFloat(vf) + parseFloat(vi)));
+                console.log(returnObj.t);
+            }
+            
+            if (vi && a && d && !vf){
+                returnObj.vf = Math.sqrt(parseFloat(parseFloat(vi * vi) + parseFloat(2*a*d)));
+            }
+            else if (a && d && vf && !vi){
+                returnObj.vi = Math.abs(parseFloat(parseFloat(vf*vf) - parseFloat(2*a*d)));
+            }
+            else if (d && vf && vi && !a){
+                returnObj.a = parseFloat(parseFloat(vf*vf) - parseFloat(vi*vi)) / parseFloat(2*d); 
+            }
+            else if (vf && vi && a && !d){
+                returnObj.d = parseFloat(parseFloat(vf*vf) - parseFloat(vi*vi)) / parseFloat(2*a); 
+            }
+            
+            
+        
+        console.log(returnObj);
+            var runningTotal = 0;
+            for (var key in returnObj){
+                if(!!returnObj[key] || returnObj[key] === 0)
+                    runningTotal++;
+            }
+            if (total === runningTotal){
+                console.log(runningTotal, total);
+                somethingNew = false;
+            }
+            total = runningTotal;
+            
         }
         
-        if (vi && vf && t){
-            returnObj.d = parseFloat(parseFloat(parseFloat(vi) + parseFloat(vf)) / 2) * parseFloat(t);
+        if (total === 5){
+            console.log(!!returnObj.t)
+            res.send(returnObj);
         }
-        else if (vf && t && d){
-            returnObj.vi = parseFloat(parseFloat(parseFloat(2 * d) / parseFloat(t)) - parseFloat(vf));
+        else {
+            res.send(false);
         }
-        else if (t && d && vi){
-            returnObj.vf = parseFloat(parseFloat(parseFloat(2 * d) / parseFloat(t)) - parseFloat(vi));
-        }
-        else if (d && vi && vf){
-            returnObj.t = parseFloat(parseFloat(parseFloat(2 * d) / parseFloat(parseFloat(vf)) + parseFloat(vi)));
-        }
-        
-        if (vi && a && d){
-            returnObj.vf = Math.sqrt(parseFloat(parseFloat(vi * vi) + parseFloat(2*a*d)));
-        }
-        else if (a && d && vf){
-            returnObj.vi = Math.abs(parseFloat(parseFloat(vf*vf) - parseFloat(2*a*d)));
-        }
-        else if (d && vf && vi){
-            returnObj.a = parseFloat(parseFloat(vf*vf) - parseFloat(vi*vi)) / parseFloat(2*d); 
-        }
-        else if (vf && vi && a){
-            returnObj.d = parseFloat(parseFloat(vf*vf) - parseFloat(vi*vi)) / parseFloat(2*a); 
-        }
-        
-        
-        res.send(returnObj);
     }
     else {
         res.sendStatus(400);
